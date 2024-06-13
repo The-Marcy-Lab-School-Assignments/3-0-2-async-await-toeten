2ee599a22d56817bb117575f7c3f62c55f0fb098
toeten <n.santiago0228@gmail.com>
2024-06-13 15:26:37 -0400

    answered qs

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,8 +1,15 @@
 export const fetchHandler = async (url, options = {}) => {
   try {
-    const fetchCall = await fetch(url, options)
+    const response = await fetch(url, options)
     if (!response.ok) throw new Error(`Fetch failed with status - ${response.status}, ${response.statusText}`)
-    return [{}, null]
+
+    const isJson = (response.headers.get('content-type') || '').includes('application/json');
+    if(!isJson) {
+      const textData = await response.text();
+      return [textData, null]
+    }
+    const jsonData = await response.json()
+    return [jsonData, null]
   } catch(error) {
     console.warn(error)
     return [null, error]
